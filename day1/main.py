cb3d09ce58d0df85ed8acc87d127a853456bae0e
Nikola Đipanov <nikola.djipanov@gmail.com>
2021-12-01 10:48:49 +0000

    day1 cleanup

--- day1/main.py
+++ day1/main.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python3
+from itertools import pairwise
+from more_itertools import triplewise
 
-from collections import deque
 
 def increases(ms):
-    prev = None
-    inc = 0
-    for m in ms:
-        if prev is not None and m > prev:
-            inc += 1
-        prev = m
-    return inc
-
-def increases_w(ms):
-    w = deque(ms[:3])
-    ms = ms[3:]
-    inc = 0
-    cur = sum(w)
-    for m in ms:
-        lft = w.popleft()
-        nxt = cur - lft + m
-        if nxt > cur:
-            inc += 1
-        cur = nxt
-        w.append(m)
-    return inc
+    return sum(1 if snd > fst else 0 for fst, snd in pairwise(ms))
 
 
 if __name__ == "__main__":
@@ -31,4 +12,4 @@ if __name__ == "__main__":
         data = f.read()
     lines = [int(line.strip()) for line in data.split("\n") if line]
     print(increases(lines))
-    print(increases_w(lines))
+    print(increases(map(sum, triplewise(lines))))
