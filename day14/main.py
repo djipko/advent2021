69dd392a13a4deb63de05dbf37b71970cf9cba27
Nikola Đipanov <nikola.djipanov@gmail.com>
2021-12-14 12:53:35 +0000

    day14 - cleanup

--- day14/main.py
+++ day14/main.py
@@ -5,26 +5,6 @@ from collections import Counter
 from functools import lru_cache
 import math
 
-c = {}
-hits = 0
-miss = 0
-
-def cache(f):
-    def _inner(slf, run, cnt):
-        global hits
-        global miss
-        if cnt < 10:
-            return f(slf, run, cnt)
-        if (run, cnt) in c:
-            hits += 1
-            return c[(run, cnt)]
-        else:
-            miss += 1
-            c[(run, cnt)] = f(slf, run, cnt)
-            return c[(run, cnt)]
-
-    return _inner
-
 
 class Poly:
     def __init__(self, rules):
@@ -34,42 +14,25 @@ class Poly:
             self.rules[pair] = ins
             self.cache = {}
 
-    def step(self, pair):
-        def _build():
-                yield pair[0]
-                yield self.rules[pair]
-                yield pair[1]
-        return "".join(_build())
-
-    @cache
-    def step_run(self, run, cnt):
-        if cnt == 0:
-            return run
-        res = []
-        for pair in pairwise(run):
-            res.append(self.step_run(self.step(pair), cnt - 1))
-        #print(res)
-
-        return res[0] + "".join(c[1:] for c in res[1:])
-
-    @lru_cache(maxsize=100000)
-    def step2(self, run):
+    @lru_cache(maxsize=1024)
+    def step(self, run):
+        """This one was actually fairly OK but broke down after 30 steps"""
         l = len(run)
         if l == 1:
             return run
         elif l == 2:
             return f"{run[0]}{self.rules[run]}{run[1]}"
         else:
-            fst, snd = self.step2(run[:l//2]), self.step2(run[l//2:])
+            fst, snd = self.step2(run[: l // 2]), self.step2(run[l // 2 :])
             mid = f"{fst[-1]}{snd[0]}"
             return f"{fst}{self.rules[mid]}{snd}"
-                
-            
-        
+
+
 class Ch:
     def __init__(self, c):
         self.c = c
         self.nxt = None
+
 
 class Str:
     def __init__(self, rules, chain):
@@ -105,6 +68,7 @@ class Str:
             fst = snd
             snd = snd.nxt
 
+
 class PairPoly:
     def __init__(self, rules, template):
         self.rules = {}
@@ -133,7 +97,7 @@ class PairPoly:
         if self.start == self.end:
             c[self.start] += 1
         return c
-    
+
 
 if __name__ == "__main__":
     with open("input") as f:
@@ -142,19 +106,18 @@ if __name__ == "__main__":
     # do stuff with data
     start = lines[0]
     rules = lines[2:]
-    # poly = Poly(rules)
-    # for i in range(30):
-    #     start = poly.step2(start)
-    #     #print(start)
-    #     print(poly.step2.cache_info())
-    #res = poly.step_run(start, 30)
-    #print(hits, miss, len(c))
-    #print(res)
     s = Str(rules, start)
     for i in range(10):
         s.step()
     c = Counter(s.iter())
     print(c.most_common()[0][1] - c.most_common()[-1][1])
+
+    # poly = Poly(rules)
+    # for i in range(30):
+    #     start = poly.step(start)
+    #     print(poly.step.cache_info())
+    # c = Counter(start)
+    # print(c.most_common()[0][1] - c.most_common()[-1][1])
 
     poly = PairPoly(rules, start)
     for i in range(40):
