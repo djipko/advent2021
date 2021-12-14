84fb52437311627de42a87c1d9a505790da641d2
Nikola Đipanov <nikola.djipanov@gmail.com>
2021-12-14 12:47:32 +0000

    day14 working but wit all the attempts

--- day14/main.py
+++ day14/main.py
@@ -3,6 +3,7 @@
 from more_itertools import pairwise, chunked
 from collections import Counter
 from functools import lru_cache
+import math
 
 c = {}
 hits = 0
@@ -23,6 +24,7 @@ def cache(f):
             return c[(run, cnt)]
 
     return _inner
+
 
 class Poly:
     def __init__(self, rules):
@@ -103,27 +105,60 @@ class Str:
             fst = snd
             snd = snd.nxt
 
+class PairPoly:
+    def __init__(self, rules, template):
+        self.rules = {}
+        for rule in rules:
+            pair, ins = rule.split(" -> ")
+            self.rules[pair] = ins
+            self.cache = {}
+        self.pairs = Counter("".join(pair) for pair in pairwise(template))
+        self.start = template[0]
+        self.end = template[-1]
+
+    def step(self):
+        new_pairs = Counter()
+        for pair, cnt in self.pairs.items():
+            ins = self.rules[pair]
+            new_pairs[f"{pair[0]}{ins}"] += cnt
+            new_pairs[f"{ins}{pair[1]}"] += cnt
+        self.pairs = +new_pairs
+
+    def count(self):
+        c = Counter()
+        for pair, cnt in self.pairs.items():
+            c[pair[0]] += cnt
+            c[pair[1]] += cnt
+        c = Counter({ch: math.ceil(cnt / 2) for ch, cnt in c.items()})
+        if self.start == self.end:
+            c[self.start] += 1
+        return c
+    
 
 if __name__ == "__main__":
-    with open("input.test") as f:
+    with open("input") as f:
         data = f.read().strip()
     lines = [line.strip() for line in data.split("\n")]
     # do stuff with data
     start = lines[0]
     rules = lines[2:]
-    poly = Poly(rules)
-    for i in range(10):
-        start = poly.step2(start)
-        #print(start)
-        print(poly.step2.cache_info())
+    # poly = Poly(rules)
+    # for i in range(30):
+    #     start = poly.step2(start)
+    #     #print(start)
+    #     print(poly.step2.cache_info())
     #res = poly.step_run(start, 30)
     #print(hits, miss, len(c))
     #print(res)
-    #s = Str(rules, start)
-    #for i in range(40):
-    #    print(i)
-    #    s.step()
-    #    #print("".join(s.iter()))
+    s = Str(rules, start)
+    for i in range(10):
+        s.step()
+    c = Counter(s.iter())
+    print(c.most_common()[0][1] - c.most_common()[-1][1])
 
-    c = Counter(start)
+    poly = PairPoly(rules, start)
+    for i in range(40):
+        poly.step()
+
+    c = poly.count()
     print(c.most_common()[0][1] - c.most_common()[-1][1])
